166a479512def464ce7af6cb8c5c056193a0ed04
Abdullah Atif <152883705+abdullahatif07@users.noreply.github.com>
2026-02-22 10:34:23 +0530

    Update SecureStudentLoginSystem.java
    Final Code as of now will be implementing exception handling in future

--- SecureStudentLoginSystem.java
+++ SecureStudentLoginSystem.java
@@ -1,54 +1,93 @@
-import java.util.Scanner;
+// Project Title: Secure Student Login System with Arithmetic Module
 
+import java.util.Scanner;
 public class SecureStudentLoginSystem{
     public static void main(String[] args) {
-        //creating object of Scanner class
+        //Creating object of Scanner class
         Scanner sc = new Scanner(System.in);
-        String username = "atif07";
-        String password = "atif098";
+        
+        //Storing predefined credentials
+        String predefinedUsername = "atif07";
+        String predefinedPassword = "atif@dce07";
 
+        //Asking user to enter username and password
         System.out.print("Enter Username: ");
         String userEnteredUsername = sc.next();
+
         System.out.print("Enter Password: ");
         String userEnteredPassword = sc.next();
 
-        if(username.equals(userEnteredUsername) && password.equals(userEnteredPassword)){
-            System.out.println("\nLogin Successful!");
-        }else{
-            System.out.println("\nInvalid username or password!");
+        //Checking whether the username and password is correct or not which user has entered
+        if(!login(predefinedUsername, predefinedPassword, userEnteredUsername, userEnteredPassword)){
+            sc.close();
             return;
         }
 
-        
+        //Performing Arithmetic Operation
+        while(true){
+            //Displaying Menu
+            System.out.println("\n---- MENU ----");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Exit");
 
-        System.out.print("Enter First Number: ");
-        int firstNumber = sc.nextInt();
-        System.out.print("Enter Second Number: ");
-        int secondNumber = sc.nextInt();
+            //Taking user choice
+            System.out.print("\nEnter choice: ");
+            int choice = sc.nextInt();
 
-        System.out.println("1. Addition");
-        System.out.println("2. Subtraction");
-        System.out.println("3. Multiplication");
-        System.out.println("4. Exit");
+            //checking whether user wants to exit
+            if(choice == 4){
+                System.out.println("Program terminated successfully.");
+                break;   
+            }
+            //Checking whether user has entered invalid choice
+            if(choice < 1 || choice > 3){
+                System.out.println("Invalid choice!");
+                continue;
+            }
 
-        System.out.print("Enter choice: ");
-        int choice = sc.nextInt();
+            //Taking two numbers as input
+            System.out.print("Enter First Number: ");
+            int firstNumber = sc.nextInt();
 
-        int ans = 0;
-        switch (choice) {
-            case 1:
-                ans = firstNumber + secondNumber;
-                break;
-            case 2:
-                ans = firstNumber - secondNumber;
-                break;
-            case 3:
-                ans = firstNumber * secondNumber;
-                break;
-            default:
-                break;
+            System.out.print("Enter Second Number: ");
+            int secondNumber = sc.nextInt();
+
+            //Storing result
+            int res = arithmeticOperations(firstNumber, secondNumber, choice);
+            //Displaying result
+            System.out.println("Result = " + res);
+        }
+        //Closing scanner
+        sc.close();
+    }
+
+    //Method to check whether the username and password which user has entered is matching the predefined username and password or not
+    static boolean login(String predefinedUsername, String predefinedPassword, String userEnteredUsername, String userEnteredPassword){
+
+        if(userEnteredUsername.equals(predefinedUsername) && userEnteredPassword.equals(predefinedPassword)){
+            System.out.println("\nLogin Successful");
+            return true;
+        }else{
+            System.out.println("\nAccess Denied");
+            return false;
         }
 
-        System.out.println("Result = " + ans);
+    }
+
+    //Method for Performing Arithmetic Operation according to the user choice
+    static int arithmeticOperations(int num1, int num2, int choice){
+        switch (choice) {
+            case 1:
+                return num1 + num2;
+            case 2:
+                return num1 - num2;
+            case 3:
+                return num1 * num2;
+        }
+
+        return 0;//this will never execute
     }
 }
+
