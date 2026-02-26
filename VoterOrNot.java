7822f24a34be57cc925c3ed18df2d5390fc58514
Abdullah Atif <152883705+abdullahatif07@users.noreply.github.com>
2026-02-26 22:07:01 +0530

    Update VoterOrNot.java
    Added code for this project

--- VoterOrNot.java
+++ VoterOrNot.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class VoterOrNot{
+    public static void main(String[] args) {
+        //Creating Object of Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        //Taking input
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        //Storing result 
+        boolean res = isVoter(age);
+
+        //Displaying according to the result
+        if(res){
+            System.out.println("You are a Voter!");
+        }else{
+            System.out.println("You are not a Voter!");
+        }
+
+        sc.close();
+    }
+
+    //Method to check whethe the age is greater than equal to 18 or not
+    static boolean isVoter(int age){
+        return age >= 18;
+    }
+}
