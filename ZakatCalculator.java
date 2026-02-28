35321b724fbaa707495727a48132d054d2a6118a
Abdullah Atif <152883705+abdullahatif07@users.noreply.github.com>
2026-02-28 22:47:28 +0530

    Update ZakatCalculator.java
    Added full code for Zakat Calculator  It will take total wealth and then it will give zakat amount to be paid.

--- ZakatCalculator.java
+++ ZakatCalculator.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+//Program to Calculate Zakat amount
+public class ZakatCalculator{
+    public static void main(String[] args) {
+        //Creating Object of Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        //Taking total Wealth as input
+        System.out.print("Enter your total wealth: ");
+        double wealth = sc.nextDouble();
+
+        //Calculating Zakat amount
+        double zakatAmount = (wealth * 2.5) / 100.0;
+
+        //Displaying Zakat Amount
+        System.out.println("Zakat Amount = " + zakatAmount);
+
+        //Closing Scanner object
+        sc.close();
+    }
+}
